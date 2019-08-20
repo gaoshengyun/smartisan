94fabdd476c7fb387ebe06c8c2ce9a6e1cf38d1f
gaoshengyun <18616242909@163.com>
2019-08-20 14:33:22 +0800

    add css reset file

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router/index.js'
 import store from './store'
 
+import './assets/styles/reset.scss'
+
 Vue.config.productionTip = false
 
 new Vue({
