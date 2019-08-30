2ce4aaeaeb7b038a9d4c2ffe0bff929e783277bf
gaoshengyun <18616242909@163.com>
2019-08-30 18:40:03 +0800

    error404

--- src/router/routes.js
+++ src/router/routes.js
@@ -13,5 +13,10 @@ export default [
     path: '/mcenter',
     name: 'mcenter',
     component: () => import(/* webpackChunkName: "mcenter" */ '../views/MCenter.vue')
+  },
+  {
+    path: '*',
+    name: 'error404',
+    component: () => import(/* webpackChunkName: "error404" */'../views/error404.vue')
   }
 ]
---
+++ src/views/error404.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>error-404</h1>
+    <div>this page is not found</div>
+  </div>
+</template>
